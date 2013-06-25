b97fb47610d187c381e9e9d702cd0f315bf08edf
Barry <horgan.barry@gmail.com>
2013-06-25 22:24:34 +1000

    added session code

--- storeadmin/admin_login.php
+++ storeadmin/admin_login.php
@@ -1,3 +1,35 @@
+<?php 
+session_start();
+if(isset($_SESSION["manager"])){
+	header("location:index.php");
+	exit();
+}
+?>
+<?php 
+//parse the log in form if the user has filles it out and pressed 'Log In'
+if(isset($_POST["username"])&& isset($_POST["password"])){
+	$manager = preg_replace('#(^A-Za-z0-9)#i','',$_SESSION["manager"]); //filter everything out except numbers and letters
+	$password = preg_replace('#(^A-Za-z0-9)#i', '', $_SESSION["password"]); //filter everything out except numbers and letters
+	//connect to mySQl db
+	include 'storescripts/connect_to_mysql.php';
+	$sql = mysql_query("SELECT  id FROM admin WHERE username='$manager' AND password='$password' LIMIT 1");
+	//MAKE SURE person exists on db
+	$existCount = mysql_num_rows($sql); //count num of rows
+	if($existCount == 1){
+		while ($row = mysql_fetch_array($sql)){
+			$id = $row["id"];
+		}
+		$_SESSION["id"]=$id;
+		$_SESSION["manager"]=$manager;
+		$_SESSION["password"]=$password;
+		header("location:index.php");
+		exit();
+	}else {
+		echo 'That information is incorrect, please try again. <a href="index.php">Click here</a>';
+		exit(); 
+	}
+}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
